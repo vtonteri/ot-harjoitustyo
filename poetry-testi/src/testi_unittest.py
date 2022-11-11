59eca004b23d6f0c5bd318bb8fbe9dee636e96bc
Ville <ville.tonteri@helsinki.fi>
2022-11-11 09:25:25 +0200

    poetry-testi

---
+++ poetry-testi/src/testi_unittest.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('dsaog'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('foo'.isupper())
+
+    def test_split(self):
+        s = 'hello kulli'
+        self.assertEqual(s.split(), ['hello', 'kulli'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(0)
+
+if __name__ == '__main__':
+    unittest.main()
