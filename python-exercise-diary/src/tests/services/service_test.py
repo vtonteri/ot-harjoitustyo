bd3cdaf39d3a97faae8bfac028219bb599697bad
Ville <ville.tonteri@helsinki.fi>
2022-11-16 19:58:01 +0200

    python-exercise-diary

---
+++ python-exercise-diary/src/tests/services/service_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from service.workout_service import WorkoutService
+
+class TestWorkoutService(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_weight_adjust(self):
+        pass
