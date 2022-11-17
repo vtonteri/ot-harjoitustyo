f067aff93314c5228de986b42ff8f6c3f8a1a0af
Ville <ville.tonteri@helsinki.fi>
2022-11-17 09:18:00 +0200

    workout_diary

---
+++ workout_diary/dokumentaatio/tuntikirjanpito.md
@@ -0,0 +1,8 @@
+# Työaikakirjanpito
+
+
+| Päivä | Aika | Mitä tein |
+| --- | --- | --- |
+| 12.11. | 2 | Määrittelydokumentti |
+| 16.11. | 3 | Ohjelmarungon koodaus
+| Yht | 5 |  |
---
+++ workout_diary/src/entities/workout.py
@@ -0,0 +1,42 @@
+from user import User
+
+class WorkOut:
+    """Class for a one workout object. 
+    
+    Attributes: 
+        - Workout name
+        - Workout repetition
+            - Weekly or monthly
+        - Workout type
+            - Weight lifting or cardio
+        - Workout details, weight lifting
+            - Progressive or maintaining
+            - Sets
+                - Name
+                - Repetitions
+                - Weights
+        - Workout details, cardio
+            - Type: Running, Swimming, Cycling, Walking or User defined
+            - Duration
+            - Heart rate zone
+                - Moderate activity
+                - Weight Control
+                - Aerobic
+                - Anaerobic
+                - VO2 Maximum    
+    """
+    def __init__(self, name: str, repetition: bool, workout_type: str, workout_id:None):
+
+        self.workout_name = name
+        self.workout_repetition = repetition #Weekly: True, Monthly: False
+        self.workout_type = workout_type
+        self.workout_details_weight_lifting = {}
+        self.workout_sets_weight_lifting = {}
+        self.workout_details_cardio = {}
+        self.workout_id = workout_id
+
+    def add_weight_lifting_set(self, name, repetitions, weight):
+        self.workout_sets_weight_lifting[name] = (repetitions, weight)
+
+
+
