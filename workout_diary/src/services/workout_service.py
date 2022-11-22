205811f4609d47b999fe3d34c71e8cb40d79e76a
Ville <ville.tonteri@helsinki.fi>
2022-11-22 12:55:41 +0200

    workout_diary

--- workout_diary/src/services/workout_service.py
+++ workout_diary/src/services/workout_service.py
@@ -1,16 +1,25 @@
-from .entities.user import User
-from .entities.workout import Workout
+#from entities.user import User
+#from entities.workout import Workout
 
 class WorkoutService:
 
     def __init__(self) -> None:
         pass
         
-    def create_weight_lift_workout(self):
+    def create_workout(self, name: str, repetition: bool, workout_type: str, workout_id:None):
+
+        #if workout_type == "weight_lift":
+        #    workout = Workout(name, repetition, workout_type, workout_id)
+        #elif workout_type == "cardio":
+        #    workout = Workout(name, )
+
+        #Workout is saved in the database
+        #workout id links it to a certain user
         pass
 
     def create_cardio_workout(self):
-        pass    
+        #cardio = Workout
+        pass
     
     def add_weight_lifting_set(self, name, repetitions, weight):
         self.workout_sets_weight_lifting[name] = (repetitions, weight)
--- workout_diary/src/tests/services/service_test.py
+++ workout_diary/src/tests/services/service_test.py
@@ -1,13 +1,13 @@
 import unittest
 
-from ..services.workout_service import WorkoutService
+from services.workout_service import WorkoutService
 #from ..entities.user import User
 #from ..entities.workout import Workout
 
 class TestWorkoutService(unittest.TestCase):
     def setUp(self):
-        pass
+        self.workout = WorkoutService()
 
     def test_weight_adjust(self):
-        answer = WorkoutService.adjust_weights(100,10)
+        answer = WorkoutService.adjust_weights(self, 100, 10)
         self.assertEqual(answer, 110)
